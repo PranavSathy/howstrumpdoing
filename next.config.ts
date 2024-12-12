a98002b65cbd68f2d5bfbc1f79d6bfb6be3942eb
Pranav Sathyanarayanan <pranav@reverecre.com>
2024-12-11 19:57:12 -0500

    Enable static builds

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  // see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+  output: "export",
 };
 
 export default nextConfig;
